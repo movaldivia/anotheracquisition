347d15bece5177e08ac8561c1c9727e3b45ed2ee
movaldivia <movaldivia@uc.cl>
2023-12-08 19:42:29 -0300

    feat: events 100% coverage

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -3,18 +3,64 @@ include ActionController::RespondWith
 
 RSpec.describe 'Whether access is ocurring properly', type: :request do
   before(:each) do
-    # @current_user = FactoryBot.create(:user)
     @user = FactoryBot.create(:user)
+    @event = FactoryBot.create(:event)
   end
 
-  context 'context: general authentication via API, ' do
-    it "doesn't give you anything if you don't log in" do
+  context 'context: events operations ' do
+    it "only autheticated users can query events" do
       get '/api/events'
       expect(response.status).to eq(401)
     end
 
-    it 'user can get the events info' do
+    it 'user can query events' do
       get '/api/events', headers: @user.create_new_auth_token
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'user can query events that has joined' do
+      get "/api/events?status=JOINED", headers: @user.create_new_auth_token
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'user can create a new event' do
+      params = {           name: "An event",
+      description: "description",
+      organizer: "organizer",
+      location: "location",
+      datetime: DateTime.now, }
+      post '/api/events', headers: @user.create_new_auth_token, params: params,as: :json
+
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'user can get event info' do
+      get "/api/events/#{@event.id}", headers: @user.create_new_auth_token
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'user can join a event' do
+      params = {           id: @event.id,}
+      post '/api/events/join', headers: @user.create_new_auth_token, params: params,as: :json
+      expect(response).to have_http_status(:success)
+    end
+
+    # To do validate that user has joined before
+    it 'user can unjoin a event' do
+      params = {           id: @event.id,}
+      post '/api/events/unjoin', headers: @user.create_new_auth_token, params: params,as: :json
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'user can update event' do
+      params = { id: @event.id, name: 'new_name'}
+      put '/api/events', headers: @user.create_new_auth_token, params: params, as: :json
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'user can destroy event' do
+      params = { id: @event.id}
+      delete '/api/events', headers: @user.create_new_auth_token, params: params, as: :json
       expect(response).to have_http_status(:success)
     end
 
